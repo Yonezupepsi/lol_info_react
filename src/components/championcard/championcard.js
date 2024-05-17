a181b73a2944a4a1087beb3e413fd48ba6af3469
Yonezupepsi <ghwlseotls@gmail.com>
2024-05-17 17:10:05 +0900

    커밋!

--- src/components/championcard/championcard.js
+++ src/components/championcard/championcard.js
@@ -1,18 +1,18 @@
 import React from "react";
 import {Container, Text } from "./styled";
 
-export const ChampionCard = ({src, name}) => {// 프롭스를 통해 받아온 데이터
-  
+export const ChampionCard = ({src, name, id, onDelete}) => {// 프롭스를 통해 받아온 데이터
+  const handleClick = () => {
+    onDelete(id); // champion페이지에서 카드를 클릭시 onDelete 콜백을 호출하고 챔피언의 id를 전달.
+  };
     return (    
-          <Container className="ChampionCard">
-            <button >
+          <Container className="ChampionCard" onClick={handleClick}>
               <img
                 style = {{ width: '200px', height: '200px', borderRadius: '20px' }}// 이미지 사이즈를 인라인 스타일로 지정
                 alt = "당신의 챔피언"
                 src = {src}
               />
               <Text>{name}</Text>
-            </button>
           </Container>
         
       );
--- src/pages/champion/champion.js
+++ src/pages/champion/champion.js
@@ -1,31 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ChampionCard } from "../../components/championcard/championcard";
 import { Container, PageContainer, Button } from "./styled.js";
 
 export const Champion = () => {
-    const delChampion = () => {
-        JSON.parse(localStorage.removeItem('championData'));
-      };
+    // useState를 사용하여 champions 상태를 선언하고 저장.
+const [champions, setChampions] = useState([]);
+
+// useEffect를 사용하여 컴포넌트가 처음 렌더링될 때 한 번만 실행되는 효과를 줍니다.
+// 이후 localStorage에서 챔피언 데이터를 가져와서 champions 상태를 업데이트합니다.
+useEffect(() => {
+    // localStorage에서 챔피언 데이터를 가져옵니다. 
+    // 만약 localStorage에 데이터가 없다면 기본값으로 빈 배열을 설정합니다.
+    const getChampionsLS = JSON.parse(localStorage.getItem('championData')) || [];
+    // 가져온 챔피언 데이터를 설정하여 컴포넌트를 업데이트합니다.
+    setChampions(getChampionsLS);
+}, []);
     const getChampions = JSON.parse(localStorage.getItem('championData'));// 로컬스토리지에 데이터를 가지고 온다. 문자열로 받았기 때문에 parse로 객체 변환.
     const clearData = () => {// 로컬스토리지에 저장 된 데이터를 모두 삭제하기 위한 함수 작성 클리어가 된 후에는 다시 메인페이지로 이동.
         localStorage.clear();
         window.location.href = '/';
     };
+    
     const noChampion = () => {// 저장된 데이터가 없을시 리턴될 함수를 작성.
         alert('저장된 챔피언이 없습니다! 챔피언을 찾으러 이동합니다!');
         window.location.href = '/info';
+    };
+    const removeChampion = (id) => {
+        const updatedChampions = champions.filter(champion => champion.id !== id);
+        setChampions(updatedChampions);
+        localStorage.setItem('championData', JSON.stringify(updatedChampions));
     };
     if (getChampions){// if문을 활용해 getChampions에 데이터가 있을 때만 map함수를 실행하게 만들어 오류를 방지한다.
     return(
        <>
        <PageContainer>
        <Container>
-        {getChampions.map((e, i) => (// map함수를 통해 ChampionCard가 가지고 있는 데이터 만큼 출력.
-            <ChampionCard onClick={delChampion}
+        {getChampions.map((champion, i) => (// map함수를 통해 ChampionCard가 가지고 있는 데이터 만큼 출력.
+            <ChampionCard
                 key={i}
-                id={e.id}
-                name={e.name}
-                src={e.src}
+                id={champion.id}
+                name={champion.name}
+                src={champion.src}
+                onDelete={() => removeChampion(champion.id)}
             />
         ))}
         </Container>
@@ -33,6 +49,7 @@ export const Champion = () => {
         <Button onClick={clearData}>clear</Button>
         </>
     )
+    
 }else{// 챔피언이 없으면 noChampion 함수를 실행
     return noChampion();
 }
